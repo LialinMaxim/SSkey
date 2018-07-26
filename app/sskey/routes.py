fd81d43588ea2a5250c82fda5bc5e9d8e62e65af
Artur <manukian.artur.94@gmail.com>
2018-07-26 13:56:28 +0300

    Deleted unnecessary route.

--- app/sskey/routes.py
+++ app/sskey/routes.py
@@ -1,32 +1,6 @@
-"""
-All routes are kept here
-"""
-
-# from flask import render_template, url_for, flash, redirect
-# from app import app
-# from app.models import User
-
-
-# @app.route("/register", methods=["POST"])
-# def register():
-#     form = RegistrationForm()
-#     if form.valide_onsubmit():
-#         flash(f"Account created for {form.username.data}!", "success")
-#         return redirect(url_for("home"))
-#     return ?
-
 from . import sskey
-from app import models
 
 
 @sskey.route('/')
 def index():
     return '<h1>Hello, Team!</h1>'
-
-
-@sskey.route('/user/<username>', methods=['GET'])
-def user(username):
-    return None
-
-
-
