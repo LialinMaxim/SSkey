952372d3a0569f6413810d1d838e321a70845545
Artur Manukian <manukian.artur.94@gmail.com>
2018-07-30 16:30:24 +0300

    Added initialization of HTTP Basic Authentication in __init__.py. Uncommented secret key config.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_restful import Api
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
 
 api = Api(app)
 
-# app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
+basic_auth = HTTPBasicAuth()
+# token_auth = HTTPTokenAuth('Bearer')
+# token_serializer = Serializer(app.config["SECRET_KEY"], expires_in=1800)
+
 # db = ...
 
 from app.errors.handlers import errors
