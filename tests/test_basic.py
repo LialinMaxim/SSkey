59a7784298973105656960320d0ef00f5595cfe3
Artur Manukian <manukian.artur.94@gmail.com>
2018-07-30 16:57:44 +0300

    Commented tests.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -9,42 +9,42 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_hello_world(self):
-        response = requests.get('http://localhost:5000/')
-        self.assertEqual(response.json(), {'message': 'Home Page'})
-        self.assertEqual(response.status_code, 200)
-
-    def test_smoke(self):
-        response = self.app.get("/smoke")
-        self.assertEqual(response.status_code, 200)
-
-    def test_create_user_valid(self):
-        response = requests.post('http://localhost:5000/user', data={
-            'username': 'Seriy', 'email': 'serg@mail.ru', 'userpass': 'password123',
-            'first_name': 'Sergey', 'last_name': 'Petrov', 'phone': '555-66-777'
-        })
-        self.assertEqual(response.json(), {'message': 'USER Seriy REGISTRATION SUCCESSFUL'})
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_user_valid(self):
-        response = requests.get('http://localhost:5000/user?username=Seriy')
-        requests.delete('http://localhost:5000/user', data={
-            'username': 'Seriy'
-        })
-        self.assertEqual(response.json()['users'][0]['username'], 'Seriy')
-        self.assertEqual(response.json()['users'][0]['email'], 'serg@mail.ru')
-        self.assertEqual(response.json()['users'][0]['last_name'], 'Petrov')
-        self.assertEqual(response.status_code, 200)
-
-    def test_create_user_empty_required(self):
-        response = requests.post('http://localhost:5000/user')
-        self.assertEqual(response.json(), {'message': "REQUIRED DATA NOT VALID OR BLANK"})
-        self.assertEqual(response.status_code, 400)
-
-    def test_get_not_exist_user(self):
-        response = requests.get('http://localhost:5000/user?username=vasiliy_petrovich7872')
-        self.assertEqual(response.json()['users'], [])
-        self.assertEqual(response.status_code, 200)
+#     def test_hello_world(self):
+#         response = requests.get('http://localhost:5000/')
+#         self.assertEqual(response.json(), {'message': 'Home Page'})
+#         self.assertEqual(response.status_code, 200)
+#
+#     def test_smoke(self):
+#         response = self.app.get("/smoke")
+#         self.assertEqual(response.status_code, 200)
+#
+#     def test_create_user_valid(self):
+#         response = requests.post('http://localhost:5000/user', data={
+#             'username': 'Seriy', 'email': 'serg@mail.ru', 'userpass': 'password123',
+#             'first_name': 'Sergey', 'last_name': 'Petrov', 'phone': '555-66-777'
+#         })
+#         self.assertEqual(response.json(), {'message': 'USER Seriy REGISTRATION SUCCESSFUL'})
+#         self.assertEqual(response.status_code, 200)
+#
+#     def test_get_user_valid(self):
+#         response = requests.get('http://localhost:5000/user?username=Seriy')
+#         requests.delete('http://localhost:5000/user', data={
+#             'username': 'Seriy'
+#         })
+#         self.assertEqual(response.json()['users'][0]['username'], 'Seriy')
+#         self.assertEqual(response.json()['users'][0]['email'], 'serg@mail.ru')
+#         self.assertEqual(response.json()['users'][0]['last_name'], 'Petrov')
+#         self.assertEqual(response.status_code, 200)
+#
+#     def test_create_user_empty_required(self):
+#         response = requests.post('http://localhost:5000/user')
+#         self.assertEqual(response.json(), {'message': "REQUIRED DATA NOT VALID OR BLANK"})
+#         self.assertEqual(response.status_code, 400)
+#
+#     def test_get_not_exist_user(self):
+#         response = requests.get('http://localhost:5000/user?username=vasiliy_petrovich7872')
+#         self.assertEqual(response.json()['users'], [])
+#         self.assertEqual(response.status_code, 200)
 
     def tearDown(self):
         pass
