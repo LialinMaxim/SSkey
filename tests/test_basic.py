d64b5dc4e697c01bfebfdee030bc1d7c0a0f1820
glushko311 <glushko311@gmail.com>
2018-07-30 10:23:10 +0300

    add test_hello_world and test_smoke

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,24 @@
 import unittest
+import manage
+# import flaskapi
+
+import requests
 
 
-class FlaskTestCase(unittest.TestCase):
-
+class TestFlaskApiUsingRequests(unittest.TestCase):
     def setUp(self):
         pass
+
+    def test_hello_world(self):
+        response = requests.get('http://localhost:5000/')
+        self.assertEqual(response.json(), {'message': 'Home Page'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_smoke(self):
+        response = requests.get('http://localhost:5000/smoke')
+        self.assertEqual(response.json(), {'message': 'OK'})
+        self.assertEqual(response.status_code, 200)
+
 
     def tearDown(self):
         pass
