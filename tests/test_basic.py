ed2a1df6f7b90c89bb66694de8611cdf9ab45ed7
vromanuk <romanuk_vlad@ukr.net>
2018-07-30 12:36:55 +0300

    added test structure for future tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,8 @@
 import unittest
 from manage import app
+
+
+# import db
 # from app.models import User, Password
 
 
@@ -15,16 +18,20 @@ class FlaskTestCase(unittest.TestCase):
         response = self.app.get("/smoke")
         self.assertEqual(response.status_code, 200)
 
-
-# class TestUserModel(FlaskTestCase):
-#     def test_encode_auth_token(self):
-#         user = User(username="testUser", email="test@test.com", password="test", first_name="test", last_name="test",
-#                     phone=123)
-#         db.session.add(user)
-#         db.session.commit()
-#         auth_token = user.encode_auth_token(user.id)
-#         self.assertTrue(isinstance(auth_token, bytes))
-#         self.assertTrue(User.decode_auth_token(auth_token) == 1)
+    # def test_unique_username(self):
+    #     user = User(username="test", email="test@gmail.com")
+    #     db.session.add(user)
+    #     db.session.commit()
+    #
+    #
+    # def test_encode_auth_token(self):
+    #     user = User(username="testUser", email="test@test.com", password="test", first_name="test", last_name="test",
+    #                 phone=123)
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     auth_token = user.encode_auth_token(user.id)
+    #     self.assertTrue(isinstance(auth_token, bytes))
+    #     self.assertTrue(User.decode_auth_token(auth_token) == 1)
 
 
 if __name__ == "__main__":
