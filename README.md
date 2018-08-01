1e6e5bf3da6687aaeeca09e4f123c0d7b853af90
vromanuk <romanuk_vlad@ukr.net>
2018-08-01 11:53:00 +0300

    brought pytest in the project

--- README.md
+++ README.md
@@ -66,7 +66,7 @@ docker-compose run -p 5000:5000 web python manage.py
 Standalone unit tests run with:
 
 ```shell
-python load_test.py
+python -m pytest tests/
 ```
 
 ## Postgresql
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,67 +1,61 @@
-import unittest
-import requests
+import os
+import tempfile
 
-from manage import app
+import pytest
 
-
-class TestFlaskApiUsingRequests(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-
-    def test_hello_world(self):
-        response = self.app.get("/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_smoke(self):
-        response = self.app.get("/smoke")
-        self.assertEqual(response.status_code, 200)
-
-    # def test_create_user_valid(self):
-    #     response = requests.post('http://localhost:5000/users', data={
-    #         'username': 'Seriy', 'email': 'serg@mail.ru', 'userpass': 'password123',
-    #         'first_name': 'Sergey', 'last_name': 'Petrov', 'phone': '555-66-777'
-    #     })
-    #     self.assertEqual(response.json(), {'message': 'USER Seriy REGISTRATION SUCCESSFUL'})
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_get_user_valid(self):
-    #     response = requests.get('/users?username=Seriy')
-    #     requests.delete('http://localhost:5000/user', data={
-    #         'username': 'Seriy'
-    #     })
-    #     self.assertEqual(response.json()['users'][0]['username'], 'Seriy')
-    #     self.assertEqual(response.json()['users'][0]['email'], 'serg@mail.ru')
-    #     self.assertEqual(response.json()['users'][0]['last_name'], 'Petrov')
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_create_user_empty_required(self):
-    #     response = requests.post('/users')
-    #     self.assertEqual(response.json(), {'message': "REQUIRED DATA NOT VALID OR BLANK"})
-    #     self.assertEqual(response.status_code, 400)
-    #
-    # def test_get_not_exist_user(self):
-    #     response = requests.get('/users?username=vasiliy_petrovich7872')
-    #     self.assertEqual(response.json()['users'], [])
-    #     self.assertEqual(response.status_code, 200)
-
-    def tearDown(self):
-        pass
-
-    # def test_unique_username(self):
-    #     user = User(username="test", email="test@gmail.com")
-    #     db.session.add(user)
-    #     db.session.commit()
-    #
-    #
-    # def test_encode_auth_token(self):
-    #     user = User(username="testUser", email="test@test.com", password="test", first_name="test", last_name="test",
-    #                 phone=123)
-    #     db.session.add(user)
-    #     db.session.commit()
-    #     auth_token = user.encode_auth_token(user.id)
-    #     self.assertTrue(isinstance(auth_token, bytes))
-    #     self.assertTrue(User.decode_auth_token(auth_token) == 1)
+from app import app
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def client():
+    db_fd, app.config["DATABASE"] = tempfile.mkstemp()
+    app.config["TESTING"] = True
+    client = app.test_client()
+
+    # with app.app_context():
+    #     app.init_db()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(app.config["DATABASE"])
+
+
+def test_smoke(client):
+    rv = client.get("/smoke")
+    assert b"OK" in rv.data
+
+
+def test_home_page(client):
+    rv = client.get("/")
+    assert b"Home Page" in rv.data
+#     # def test_create_user_valid(self):
+#     #     response = requests.post('http://localhost:5000/users', data={
+#     #         'username': 'Seriy', 'email': 'serg@mail.ru', 'userpass': 'password123',
+#     #         'first_name': 'Sergey', 'last_name': 'Petrov', 'phone': '555-66-777'
+#     #     })
+#     #     self.assertEqual(response.json(), {'message': 'USER Seriy REGISTRATION SUCCESSFUL'})
+#     #     self.assertEqual(response.status_code, 200)
+#     #
+#     # def test_get_user_valid(self):
+#     #     response = requests.get('/users?username=Seriy')
+#     #     requests.delete('http://localhost:5000/user', data={
+#     #         'username': 'Seriy'
+#     #     })
+#     #     self.assertEqual(response.json()['users'][0]['username'], 'Seriy')
+#     #     self.assertEqual(response.json()['users'][0]['email'], 'serg@mail.ru')
+#     #     self.assertEqual(response.json()['users'][0]['last_name'], 'Petrov')
+#     #     self.assertEqual(response.status_code, 200)
+#     #
+#     # def test_create_user_empty_required(self):
+#     #     response = requests.post('/users')
+#     #     self.assertEqual(response.json(), {'message': "REQUIRED DATA NOT VALID OR BLANK"})
+#     #     self.assertEqual(response.status_code, 400)
+#     #
+#     # def test_get_not_exist_user(self):
+#     #     response = requests.get('/users?username=vasiliy_petrovich7872')
+#     #     self.assertEqual(response.json()['users'], [])
+#     #     self.assertEqual(response.status_code, 200)
+#
+#     def tearDown(self):
+#         pass
