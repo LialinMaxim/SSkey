2d772be939bd1381ab7467dfc42bcd68cfa7cda9
vromanuk <romanuk_vlad@ukr.net>
2018-08-05 15:34:12 +0300

    unfinished /login test

--- src/tests/test_basic.py
+++ src/tests/test_basic.py
@@ -28,33 +28,9 @@ def test_smoke(client):
 def test_home_page(client):
     rv = client.get("/")
     assert b"Home Page" in rv.data
-#     # def test_create_user_valid(self):
-#     #     response = requests.post('http://localhost:5000/users', data={
-#     #         'username': 'Seriy', 'email': 'serg@mail.ru', 'userpass': 'password123',
-#     #         'first_name': 'Sergey', 'last_name': 'Petrov', 'phone': '555-66-777'
-#     #     })
-#     #     self.assertEqual(response.json(), {'message': 'USER Seriy REGISTRATION SUCCESSFUL'})
-#     #     self.assertEqual(response.status_code, 200)
-#     #
-#     # def test_get_user_valid(self):
-#     #     response = requests.get('/users?username=Seriy')
-#     #     requests.delete('http://localhost:5000/user', data={
-#     #         'username': 'Seriy'
-#     #     })
-#     #     self.assertEqual(response.json()['users'][0]['username'], 'Seriy')
-#     #     self.assertEqual(response.json()['users'][0]['email'], 'serg@mail.ru')
-#     #     self.assertEqual(response.json()['users'][0]['last_name'], 'Petrov')
-#     #     self.assertEqual(response.status_code, 200)
-#     #
-#     # def test_create_user_empty_required(self):
-#     #     response = requests.post('/users')
-#     #     self.assertEqual(response.json(), {'message': "REQUIRED DATA NOT VALID OR BLANK"})
-#     #     self.assertEqual(response.status_code, 400)
-#     #
-#     # def test_get_not_exist_user(self):
-#     #     response = requests.get('/users?username=vasiliy_petrovich7872')
-#     #     self.assertEqual(response.json()['users'], [])
-#     #     self.assertEqual(response.status_code, 200)
-#
-#     def tearDown(self):
-#         pass
+
+
+# def test_valid_login(client):
+#     rv = client.post("/login", data=dict(email="vlad@gmail.com", password="vlad"))
+#     assert rv.status_code == 200
+#     assert b"Logged in as vlad@gmail.com" in rv.data
