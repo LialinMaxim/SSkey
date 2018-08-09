e0a238e27909a34e9f29ac36b072b265d70f51ee
vromanuk <romanuk_vlad@ukr.net>
2018-08-09 22:03:43 +0300

    deleted unused code

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,16 +1,12 @@
-from flask import Flask, Blueprint
-from flask_restplus import Api, fields
+from flask import Flask
+from flask_restplus import Api
 
 from .config import config
 
 app = Flask(__name__)
 app.config.from_object(config['default'])
-api = Api(app, version='0.1.1', title='SSkey', description='A simple application to safe yours passwords',)
+api = Api(app, version='0.1.1', title='SSkey', description='A simple application to safe yours passwords', )
 
 app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
-#
-# from src.app.errors import handlers
-#
-# app.register_blueprint(errors)
 
 from . import routes
