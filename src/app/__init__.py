d03b33141cb117feff42903270919d4c53bccdec
vromanuk <romanuk_vlad@ukr.net>
2018-08-10 11:52:32 +0300

    added local environment

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,10 +4,8 @@ from flask_restplus import Api
 from .config import config
 
 app = Flask(__name__)
-from .config import config
+
 app.config.from_object(config['default'])
 api = Api(app, version='0.1.1', title='SSkey', description='A simple application to safe yours passwords')
-
-# app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
 
 from . import routes
