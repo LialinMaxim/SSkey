d5690a9a17c1e5bca03502f01263143793ae95b4
Artur Manukian <manukian.artur.94@gmail.com>
2018-08-12 22:37:47 +0300

    Returned api creation in __init__.py.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 from .config import config
+from .api import RepresentationApi
 
 app = Flask(__name__)
 app.config.from_object(config['default'])
+api = RepresentationApi(app, version='0.1.1', title='SSkey', description='A simple application to safe yours passwords')
 
 from . import routes
--- src/app/api.py
+++ src/app/api.py
@@ -3,8 +3,6 @@ import json
 from flask import make_response
 from flask_restplus import Api
 
-from . import app
-
 
 def output_json(data, code, headers=None):
     """Outputs data in JSON format
@@ -29,6 +27,3 @@ class RepresentationApi(Api):
         self.representations = {
             'application/json': output_json,
         }
-
-
-api = RepresentationApi(app, version='0.1.1', title='SSkey', description='A simple application to safe yours passwords')
