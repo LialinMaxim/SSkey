0534892d307355b43482a481a55a4349fea7b561
glushko311 <glushko311@gmail.com>
2018-08-13 14:04:24 +0300

    A

--- src/tests/test_basic.py
+++ src/tests/test_basic.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+from ..app.models import Password
 
 from src.app import app
 import pytest
@@ -14,16 +15,42 @@ def client():
     #     app.init_db()
 
     yield client
-
 #
-# def test_smoke(client):
-#     rv = client.get("/smoke")
-#     assert b"Unauthorized Access" in rv.data
+
+
+def test_password_encode_decode():
+    password = Password('vasya', 'vasya777', 1, "", "", "")
+    assert password.password != 'vasya777'
+    assert password.decrypt_password() == 'vasya777'
+
+
+def test_smoke(client):
+    rv = client.get("/smoke")
+    assert b"OK" in rv.data
 
 
 def test_home_page(client):
     rv = client.get("/home")
     assert b"Home Page" in rv.data
+
+def test_register(client):
+    rv = client.post("/register", json={
+                                        "username": "vasily11",
+                                        "userpass": "vasily11",
+                                        "email": "vasily345@vc.vc",
+                                        "first_name": "vasya",
+                                        "last_name": "petrov",
+                                        "phone": "123321"
+                                        })
+    assert b"SUCCESSFUL ADDED" in rv.data
+
+# def test_de(client):
+#     rv = client.post("/register", json={
+#                                         "username": "vasily11",
+#                                         "userpass": "vasily11",
+#                                         "email": "vasily345@vc.vc"
+#                                         })
+#     assert b"SUCCESSFUL ADDED" in rv.data
 
 # def test_valid_login(client):
 #     rv = client.post("/login", data=dict(email="vlad@gmail.com", password="vlad"))
