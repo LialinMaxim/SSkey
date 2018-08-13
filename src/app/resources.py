2a27f943167d476e1a5c46d995e6fd9419cfb7d2
Artur Manukian <manukian.artur.94@gmail.com>
2018-08-13 11:59:38 +0300

    Merge the changes.

--- src/app/resources.py
+++ src/app/resources.py
@@ -82,11 +82,8 @@ class UserListResource(Resource):
         try:
             users = session.query(User).all()
         except SQLAlchemyError:
-            return SQLAlchemyError, 500,  # headers
-        users_serialized = []
-        for user in users:
-            users_serialized.append(user.serialize)
-        return users_serialized, 200,  # headers
+            return SQLAlchemyError, 500  # Internal Server Error
+        return UserSchema(many=True).dump(users), 200  # OK
 
 
 @api.representation('/json')
@@ -169,7 +166,7 @@ class PasswordListResource(EntityListResource):
         return msg, status, headers
 
 
-user_post = api.model('Create New User', {
+user_model = api.model('Create New User', {
     'email': fields.String,
     'username': fields.String,
     'userpass': fields.String,
@@ -188,7 +185,7 @@ class Register(Resource):
     Otherwise, return 500 or 400 error
     """
 
-    @api.expect(user_post)
+    @api.expect(user_model)
     def post(self):
         json_data = request.get_json()
         if not json_data or not isinstance(json_data, dict):
