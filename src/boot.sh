ecca279c330df804afc559ac21abe8f27d0cbf72
Artur Manukian <manukian.artur.94@gmail.com>
2018-08-15 13:30:09 +0300

    Removed migrate.py execution from boot.sh script.

--- src/boot.sh
+++ src/boot.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 echo "Wait a minute until the server starts..."
 sleep 1m
-echo "STARTING MIGRATION"
-python3 app/migrate.py
 echo "STARTING SERVER"
 python3 manage.py runserver -h 0.0.0.0 # access at 192.168.99.100:5000
 #read -p "Press any key to continue... " -n 1 -
