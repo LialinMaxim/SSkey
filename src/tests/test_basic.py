81a6a8f7e4e9beaa36b8ff3737f85ac9e5de2c3a
glushko311 <glushko311@gmail.com>
2018-08-15 16:28:16 +0300

    Create test suite and fixture for set up and tear down

--- src/tests/test_basic.py
+++ src/tests/test_basic.py
@@ -9,7 +9,7 @@ from src.app import config
 from src.app.base import Base, engine
 
 
-@pytest.fixture
+@pytest.fixture  # (scope='class')
 def client():
     # app.config["TESTING"] = True
     app.config.from_object(config['testing'])
@@ -19,11 +19,10 @@ def client():
 
     yield client
 
-
-# def test_password_encode_decode():
-#     password = Password('vasya', 'vasya777', 1, "", "", "")
-#     assert password.password != 'vasya777'
-#     assert password.decrypt_password() == 'vasya777'
+def test_password_encode_decode():
+    password = Password('vasya', 'vasya777', 1, "", "", "")
+    assert password.password != 'vasya777'
+    assert password.decrypt_password() == 'vasya777'
 
 
 def register(client, email, username, password, first_name, last_name, phone):
@@ -35,6 +34,10 @@ def register(client, email, username, password, first_name, last_name, phone):
         last_name=last_name,
         phone=phone
     ), follow_redirects=True)
+
+
+def delete_user(client, user_id):
+    return client.delete("/users/" + user_id)
 
 
 def put_user(client, email, username, first_name, last_name, phone, user_id):
@@ -52,6 +55,14 @@ def get_user_by_username(client, username):
                       follow_redirects=True)
 
 
+def smoke(client):
+    return client.get("/smoke", follow_redirects=True)
+
+
+def logout(client):
+    return client.get("/logout", follow_redirects=True)
+
+
 def login(client, email, password):
     return client.post("/login", data=dict(
         email=email,
@@ -59,12 +70,24 @@ def login(client, email, password):
     ), follow_redirects=True)
 
 
-def smoke(client):
-    return client.get("/smoke", follow_redirects=True)
+def login_logout_dec(test_func):
+    """
+    Decorator to login as app.config["USERNAME"] make test and logout after test finish
+    :param test_func:
+    :return:
+    """
 
+    def wrapper(client):
+        # before test
+        login(client, app.config["EMAIL"], app.config["PASSWORD"])
+        rv = get_user_by_username(client, app.config["USERNAME"])
 
-def logout(client):
-    return client.get("/logout", follow_redirects=True)
+        test_func(client, rv)
+
+        # after test
+        logout(client)
+
+    return wrapper
 
 
 def test_home_page(client):
@@ -77,54 +100,93 @@ def test_smoke_page(client):
     assert b"You are not allowed to use this resource without logging in!" in rv.data
 
 
-def test_register(client):
-    """Make sure register works."""
-    rv = register(client, app.config["EMAIL"], app.config["USERNAME"], app.config["PASSWORD"], app.config["FIRST_NAME"],
-                  app.config["LAST_NAME"], app.config["PHONE"])
-    assert b"New user: 'testuser' is SUCCESSFUL ADDED" in rv.data
+# def test_register(client):
+#     """Make sure register works."""
+#     rv = register(client, app.config["EMAIL"], app.config["USERNAME"], app.config["PASSWORD"], app.config["FIRST_NAME"],
+#                   app.config["LAST_NAME"], app.config["PHONE"])
+#     assert b"New user: 'testuser' is SUCCESSFUL ADDED" in rv.data
 
 
-def test_get_user_by_username(client):
+@pytest.yield_fixture
+def resource(client):
+    print("setup")
     login(client, app.config["EMAIL"], app.config["PASSWORD"])
-    rv = get_user_by_username(client, app.config["USERNAME"])
-    assert bytes(app.config["EMAIL"], encoding='utf-8') in rv.data
-    assert bytes(app.config["FIRST_NAME"], encoding='utf-8') in rv.data
+
+    yield
+    print("teardown")
     logout(client)
 
+class TestUsersRoutesWithLogin:
 
-def test_put_user(client):
-    """Try to update user data for existant user"""
-    login(client, app.config["EMAIL"], app.config["PASSWORD"])
-    rv = get_user_by_username(client, app.config["USERNAME"])
+    # def setup(self):
+    #     login(client, app.config["EMAIL"], app.config["PASSWORD"])
+    #     logout(client)
 
-    user = json.loads(str(rv.data, encoding='utf-8'))
-    username = user['username']
-    user_id = user['id']
-    rv = put_user(client, user['email'], username, 'Ali', 'Alhazred', '666-666-666', user_id)
-    assert bytes(f'User {username} with id {user_id} has been successfully updated.', encoding='utf-8') in rv.data
-    logout(client)
+    #
+    # def teardown(self):
 
 
-# (client, email, username, first_name, last_name, phone, user_id)
+    def test_get_user_by_username(self, client, resource):
 
-def test_login_logout(client):
-    """Make sure login and logout works."""
+        rv = get_user_by_username(client, app.config["USERNAME"])
+        assert bytes(app.config["EMAIL"], encoding='utf-8') in rv.data
+        assert bytes(app.config["FIRST_NAME"], encoding='utf-8') in rv.data
 
-    rv = login(client, app.config["EMAIL"], app.config["PASSWORD"])
+    def test_put_user(self, client, resource):
+        """Try to update user data for existant user"""
 
-    assert b"Logged in as testuser@gmail.com" in rv.data
+        rv = get_user_by_username(client, app.config["USERNAME"])
+        user = json.loads(str(rv.data, encoding='utf-8'))
+        username = user['username']
+        user_id = user['id']
+        rv = put_user(client, user['email'], username, 'Ali', 'Alhazred', '666-666-666', user_id)
+        assert bytes(f'User {username} with id {user_id} has been successfully updated.', encoding='utf-8') in rv.data
 
-    rv = smoke(client)
-    assert b"OK" in rv.data
 
-    rv = logout(client)
-    assert b"Dropped" in rv.data
+    def test_delete_user(self, client, resource):
+        """Try to delete user data for existant user"""
 
-    rv = smoke(client)
-    assert b"You are not allowed to use this resource without logging in!" in rv.data
+        rv = get_user_by_username(client, app.config["USERNAME"])
 
-    rv = login(client, app.config["EMAIL"] + "x", app.config["PASSWORD"])
-    assert b"Could not verify your login!" in rv.data
+        user = json.loads(str(rv.data, encoding='utf-8'))
+        user_id = user['id']
+        rv = delete_user(client, str(user['id']))
 
-    rv = login(client, app.config["EMAIL"], app.config["PASSWORD"] + "x")
-    assert b"Could not verify your login!" in rv.data
+        assert bytes(f'User ID:{user_id} has been DELETED.', encoding='utf-8') in rv.data
+
+
+# def test_login_logout(client):
+#     """Make sure login and logout works."""
+#
+#     rv = login(client, app.config["EMAIL"], app.config["PASSWORD"])
+#
+#     assert b"Logged in as testuser@gmail.com" in rv.data
+#
+#     rv = smoke(client)
+#     assert b"OK" in rv.data
+#
+#     rv = logout(client)
+#     assert b"Dropped" in rv.data
+#
+#     rv = smoke(client)
+#     assert b"You are not allowed to use this resource without logging in!" in rv.data
+#
+#     rv = login(client, app.config["EMAIL"] + "x", app.config["PASSWORD"])
+#     assert b"Could not verify your login!" in rv.data
+#
+#     rv = login(client, app.config["EMAIL"], app.config["PASSWORD"] + "x")
+#     assert b"Could not verify your login!" in rv.data
+
+
+# def test_delete_user(client):
+#     """Try to delete user data for existant user"""
+#     login(client, app.config["EMAIL"], app.config["PASSWORD"])
+#     rv = get_user_by_username(client, app.config["USERNAME"])
+#
+#     user = json.loads(str(rv.data, encoding='utf-8'))
+#     user_id = user['id']
+#     rv = delete_user(client, str(user['id']))
+#
+#     assert bytes(f'User ID:{user_id} has been DELETED.', encoding='utf-8') in rv.data
+#
+#     logout(client)
