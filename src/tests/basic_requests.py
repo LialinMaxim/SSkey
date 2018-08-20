9dc4a6dcb4156679621f71efd9799fb7bcdc0940
Artur Manukian <manukian.artur.94@gmail.com>
2018-08-20 14:49:03 +0300

    Added register() request and replaced test_home_page() test with home_page() request in basic_requests.py.

--- src/tests/basic_requests.py
+++ src/tests/basic_requests.py
@@ -6,6 +6,16 @@ class BasicRequests:
         return client.get("/smoke", follow_redirects=True)
 
     @staticmethod
-    def test_home_page(client):
-        rv = client.get("/home")
-        assert b"Home Page" in rv.data
+    def home_page(client):
+        assert client.get("/home", follow_redirect=True)
+
+    @staticmethod
+    def register(client, email, username, password, first_name, last_name, phone):
+        return client.post("/register", json=dict(
+            email=email,
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone
+        ), follow_redirects=True)
