053171ce820e0ee08805abe669bdc7ef59061d36
Artur Manukian <manukian.artur.94@gmail.com>
2018-08-20 15:10:54 +0300

    Added UserResource in routes for application api .

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,7 +1,7 @@
 from flask import g
 
 from . import api
-from .resources import (Home, Smoke, AdminUsersResource, PasswordResource,
+from .resources import (Home, Smoke, UserResource, AdminUsersResource, PasswordResource,
                         AdminUsersListResource, PasswordListResource, Register, Login, Logout, UserSearch)
 from .base import Session
 
@@ -10,6 +10,7 @@ api.add_resource(Smoke, '/smoke')  # GET
 api.add_resource(Login, '/login')  # POST
 api.add_resource(Logout, '/logout')  # GET
 api.add_resource(Register, '/register')  # POST
+api.add_resource(UserResource, '/<string:username>')  # GET, PUT, DELETE
 api.add_resource(AdminUsersListResource, '/admin/users')  # GET
 api.add_resource(AdminUsersResource, '/admin/users/<int:user_id>')  # GET, PUT, DELETE
 api.add_resource(PasswordListResource, '/users/<int:user_id>/passwords')  # POST, GET
