20ab038741fc3b6b95ea2a6272d96ced3c9a623f
Artur Manukian <manukian.artur.94@gmail.com>
2018-08-20 12:54:11 +0300

    Added missing slash for 'admin' in routes.py.

--- src/app/routes.py
+++ src/app/routes.py
@@ -10,8 +10,8 @@ api.add_resource(Smoke, '/smoke')  # GET
 api.add_resource(Login, '/login')  # POST
 api.add_resource(Logout, '/logout')  # GET
 api.add_resource(Register, '/register')  # POST
-api.add_resource(AdminUsersListResource, 'admin/users')  # GET
-api.add_resource(AdminUsersResource, 'admin/users/<int:user_id>')  # GET, PUT, DELETE
+api.add_resource(AdminUsersListResource, '/admin/users')  # GET
+api.add_resource(AdminUsersResource, '/admin/users/<int:user_id>')  # GET, PUT, DELETE
 api.add_resource(PasswordListResource, '/users/<int:user_id>/passwords')  # POST, GET
 api.add_resource(PasswordResource, '/users/<int:user_id>/passwords/<int:pass_id>')  # GET, PUT, DELETE
 api.add_resource(UserSearch, '/users/<string:username>')  # GET
