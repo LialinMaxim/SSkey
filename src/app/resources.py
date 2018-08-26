74e2e443ca6cbf2c1c43c8d05bdfd18e8c39bd6e
glushko311 <glushko311@gmail.com>
2018-08-27 00:11:36 +0300

    Fix tests with admin check

--- src/app/resources.py
+++ src/app/resources.py
@@ -31,7 +31,7 @@ def require_login():
     """
 
     admin_endpoints = ['admin_admin_users', 'admin_admin_users_search_list', 'admin_admin_users_number',
-                    'admin_admin_users_search']
+                       'admin_admin_users_search']
     if request.endpoint in admin_endpoints:
         user = get_user_by_token()
         if not user.is_admin:
