79d3b638ff5fffbeaca8fc5270385396187e40bd
LialinMaxim <idya.monster@gmail.com>
2018-08-29 14:11:40 +0300

    heroku update test

--- src/app/base.py
+++ src/app/base.py
@@ -14,8 +14,6 @@ engine = create_engine('postgresql://%s:%s@%s/%s' % (POSTGRES_USER,
                                                      POSTGRES_NAME
                                                      ))
 
-# engine = create_engine("sqlite://")
-
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
