c4ae54015f6eb98ebfd33575587a9a92150efd73
vromanuk <romanuk_vlad@ukr.net>
2018-09-03 16:24:15 +0300

    future tasks

---
+++ src/app/errors.py
@@ -0,0 +1,10 @@
+errors = {
+    'UserAlreadyExistError': {
+        'message': "A user with that username already exists.",
+        'status': 409
+    },
+    'ValidationError': {
+        'message': "Unprocessable Entity",
+        'status': 422
+    }
+}
