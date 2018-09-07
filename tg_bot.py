b1bb50dcd35c4a0ab958378969403bb580b5ef58
vromanuk <romanuk_vlad@ukr.net>
2018-09-07 09:46:09 +0300

    created adding new passwords in tg bot

--- tg_bot.py
+++ tg_bot.py
@@ -15,12 +15,22 @@ url = 'http://127.0.0.1:5000/'
 print(bot.get_me())
 cookies = ''
 user_dict = dict()
+pass_dict = dict()
 
 
 class UserCredentials:
     def __init__(self, email):
         self.email = email
         self.password = None
+
+
+class PasswordCredentials:
+    def __init__(self, n_url):
+        self.url = n_url
+        self.title = None
+        self.login = None
+        self.password = None
+        self.comment = None
 
 
 def log(message, answer):
@@ -34,32 +44,14 @@ def log(message, answer):
 def handle_help_command(message):
     if not cookies:
         user_markup = telebot.types.ReplyKeyboardMarkup()
-        user_markup.row('/home', '/smoke')
         user_markup.row('/login', '/help')
         bot.send_message(message.from_user.id, 'Please, choose what you\'d like to do', reply_markup=user_markup)
     if cookies:
         user_markup = telebot.types.ReplyKeyboardMarkup()
         user_markup.row('/get_me', '/get_my_pass')
-        user_markup.row('/add_my_pass', '/logout')
+        user_markup.row('/search', '/logout')
+        user_markup.row('/add_pass', '/change_pass_info')
         bot.send_message(message.from_user.id, 'Please, choose what you\'d like to do', reply_markup=user_markup)
-
-
-@bot.message_handler(commands=['home'])
-def handle_home_command(message):
-    try:
-        rv = requests.get(url + 'home')
-        bot.send_message(message.from_user.id, rv)
-    except Exception as err:
-        bot.reply_to(message, err)
-
-
-@bot.message_handler(commands=['smoke'])
-def handle_smoke_command(message):
-    try:
-        rv = requests.get(url + 'smoke', cookies=cookies)
-        bot.send_message(message.from_user.id, rv)
-    except Exception as err:
-        bot.reply_to(message, err)
 
 
 @bot.message_handler(commands=['login'])
@@ -67,7 +59,7 @@ def handle_login_command(message):
     try:
         msg = bot.reply_to(message, """\
         Hi there, I am SSkey bot.
-        Please, enter your email?
+        Please, enter your email in order to login
         """)
         bot.register_next_step_handler(msg, get_email)
     except Exception as err:
@@ -80,7 +72,7 @@ def get_email(message):
         email = message.text
         user = UserCredentials(email)
         user_dict[chat_id] = user
-        msg = bot.reply_to(message, 'Enter your password?')
+        msg = bot.reply_to(message, 'Almost there. Please, enter your password')
         bot.register_next_step_handler(msg, get_pass)
     except Exception as err:
         bot.reply_to(message, err)
@@ -132,6 +124,120 @@ def handle_get_command(message):
         bot.send_message(message.from_user.id, rv)
     except Exception as err:
         bot.reply_to(message, err)
+
+
+@bot.message_handler(commands=['search'])
+def handle_get_command(message):
+    try:
+        msg = bot.reply_to(message, """\
+                Please, enter a description of your passport in order to find the password
+                """)
+        bot.register_next_step_handler(msg, search_pass)
+    except Exception as err:
+        bot.reply_to(message, err)
+
+
+def search_pass(message):
+    try:
+        condition = message.text
+        rv = requests.post(url + 'user/passwords/search', json=dict(condition=condition), cookies=cookies)
+        passwords = rv.json()
+
+        for pas in passwords.get("passwords"):
+            bot.send_message(message.from_user.id, f'{pas}')
+    except Exception as err:
+        bot.reply_to(message, err)
+
+
+@bot.message_handler(commands=['add_pass'])
+def handle_login_command(message):
+    try:
+        msg = bot.reply_to(message, """\
+        Hi there, I am SSkey bot.
+        Please, enter an url of new password
+        """)
+        bot.register_next_step_handler(msg, get_url)
+    except Exception as err:
+        bot.reply_to(message, err)
+
+
+def get_url(message):
+    try:
+        chat_id = message.chat.id
+        n_url = message.text
+        user_pass = PasswordCredentials(n_url)
+        pass_dict[chat_id] = user_pass
+        msg = bot.reply_to(message, 'Almost there. Please, enter a title of the service')
+        bot.register_next_step_handler(msg, get_title)
+    except Exception as err:
+        bot.reply_to(message, err)
+
+
+def get_title(message):
+    try:
+        chat_id = message.chat.id
+        title = message.text
+        if not title:
+            msg = bot.reply_to(message, 'Cannot be empty.')
+            bot.register_next_step_handler(msg, get_title)
+            return
+        user_pass = pass_dict[chat_id]
+        user_pass.title = title
+        msg = bot.reply_to(message, 'A few more steps. Please, enter your login of the service')
+        bot.register_next_step_handler(msg, get_login)
+    except Exception as e:
+        bot.reply_to(message, e)
+
+
+def get_login(message):
+    try:
+        chat_id = message.chat.id
+        login = message.text
+        user_pass = pass_dict[chat_id]
+        user_pass.login = login
+        msg = bot.reply_to(message, 'Almost there. Please, enter your password')
+        bot.register_next_step_handler(msg, get_my_pass)
+    except Exception as err:
+        bot.reply_to(message, err)
+
+
+def get_my_pass(message):
+    try:
+        chat_id = message.chat.id
+        password = message.text
+        if not password:
+            msg = bot.reply_to(message, 'Cannot be empty. Please, enter your password')
+            bot.register_next_step_handler(msg, get_pass)
+            return
+        user_pass = pass_dict[chat_id]
+        user_pass.password = password
+        msg = bot.reply_to(message, 'Last, but not least. Please, enter your description for your service')
+        bot.register_next_step_handler(msg, get_description)
+    except Exception as e:
+        bot.reply_to(message, e)
+
+
+def get_description(message):
+    try:
+        chat_id = message.chat.id
+        comment = message.text
+        if not comment:
+            msg = bot.reply_to(message, 'Cannot be empty.')
+            bot.register_next_step_handler(msg, get_description)
+            return
+        user_pass = pass_dict[chat_id]
+        user_pass.comment = comment
+
+        rv = requests.post(url + 'user/passwords', json=dict(
+            url=user_pass.url,
+            title=user_pass.title,
+            login=user_pass.login,
+            password=user_pass.password,
+            comment=user_pass.comment
+        ), cookies=cookies)
+        bot.send_message(message.from_user.id, rv)
+    except Exception as e:
+        bot.reply_to(message, e)
 
 
 @bot.message_handler(content_types=['text'])
