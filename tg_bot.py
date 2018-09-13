0c5f6540db667cc530fec68bcdbc7b3866c9e2f1
vromanuk <romanuk_vlad@ukr.net>
2018-09-13 16:36:13 +0300

    changed repr of profile, cookies are linked with current user

--- tg_bot.py
+++ tg_bot.py
@@ -15,7 +15,6 @@ url = 'http://127.0.0.1:5000/'
 # url = 'http://sskey.herokuapp.com/'
 
 print(bot.get_me())
-cookies = dict()
 user_dict = dict()
 pass_dict = dict()
 
@@ -24,6 +23,24 @@ class UserCredentials:
     def __init__(self, email):
         self.email = email
         self.password = None
+        self.token = None
+
+
+class UserProfile:
+    def __init__(self):
+        self.cur_email = None
+        self.username = None
+        self.first_name = None
+        self.last_name = None
+        self.phone = None
+
+    def print_user_profile(self):
+        formatted_string = 'Email — ' + self.cur_email + '\n' + \
+                           'Username — ' + self.username + '\n' + \
+                           'First Name — ' + self.first_name + '\n' + \
+                           'Last Name — ' + self.last_name + '\n' + \
+                           'Phone — ' + self.phone
+        return formatted_string
 
 
 class PasswordCredentials:
@@ -97,8 +114,7 @@ def get_pass(message):
         user = user_dict[chat_id]
         user.password = password
         rv = requests.post(url + 'login', json=dict(email=user.email, password=user.password))
-        global cookies
-        cookies['cookie'] = rv.cookies
+        user.token = rv.cookies
         rv = rv.json()
         bot.send_message(message.from_user.id, rv.get('message'))
     except Exception as err:
@@ -107,11 +123,19 @@ def get_pass(message):
 
 @bot.message_handler(commands=['profile'])
 def handle_profile_command(message):
-    if cookies:
+    if user_dict.get(message.chat.id):
         try:
-            rv = requests.get(url + 'user/', cookies=cookies.get('cookie'))
+            rv = requests.get(url + 'user/', cookies=user_dict.get(message.chat.id).token)
             rv = rv.json().get('user')
-            bot.send_message(message.from_user.id, f'{rv}')
+
+            user_data = UserProfile()
+            user_data.cur_email = rv.get('email')
+            user_data.username = rv.get('username')
+            user_data.first_name = rv.get('first_name')
+            user_data.last_name = rv.get('last_name')
+            user_data.phone = rv.get('phone')
+
+            bot.send_message(message.from_user.id, user_data.print_user_profile())
             user_markup = telebot.types.ReplyKeyboardMarkup()
             user_markup.row('/logout', '/edit_profile')
             user_markup.row('/delete_profile', '\N{House Building}')
@@ -124,10 +148,11 @@ def handle_profile_command(message):
 
 def gen_edit_markup():
     markup = InlineKeyboardMarkup()
-    markup.row_width = 4
-    markup.add(InlineKeyboardButton('First Name', callback_data=f'change_f_name'),
+    markup.row_width = 5
+    markup.add(InlineKeyboardButton('Email', callback_data=f'change_email'),
+               InlineKeyboardButton('Username', callback_data=f'change_username'),
+               InlineKeyboardButton('First Name', callback_data=f'change_f_name'),
                InlineKeyboardButton('Last Name', callback_data=f'change_l_name'),
-               InlineKeyboardButton('Email', callback_data=f'change_email'),
                InlineKeyboardButton('Phone', callback_data=f'change_phone'))
     return markup
 
@@ -135,18 +160,20 @@ def gen_edit_markup():
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == 'change_f_name':
-        bot.answer_callback_query(call.id, "Answer is Yes")
+        bot.answer_callback_query(call.id, 'Enter a new name')
     elif call.data == 'change_l_name':
-        bot.answer_callback_query(call.id, "Answer is No")
+        bot.answer_callback_query(call.id, 'Enter a new last name')
     elif call.data == 'change_email':
-        pass
+        bot.answer_callback_query(call.id, 'Enter a new email address')
     elif call.data == 'change_phone':
-        pass
+        bot.answer_callback_query(call.id, 'Enter a new phone')
+    elif call.data == 'change_username':
+        bot.answer_callback_query(call.id, 'Enter a new username')
 
 
 @bot.message_handler(commands=['edit_profile'])
 def handle_edit_profile_command(message):
-    if cookies:
+    if user_dict.get(message.chat.id):
         try:
             bot.send_message(message.chat.id, 'Choose what you wanna change', reply_markup=gen_edit_markup())
         except Exception as err:
@@ -166,9 +193,9 @@ def handle_delete_profile_command(message):
 
 @bot.message_handler(commands=['get_passwords'])
 def handle_get_passwords_command(message):
-    if cookies:
+    if user_dict.get(message.chat.id):
         try:
-            rv = requests.get(url + 'user/passwords', cookies=cookies.get('cookie'))
+            rv = requests.get(url + 'user/passwords', cookies=user_dict.get(message.chat.id).token)
             passwords = rv.json()
             counter = 0
             results = ''
@@ -188,18 +215,21 @@ def handle_get_passwords_command(message):
 
 @bot.message_handler(commands=['logout'])
 def handle_logout_command(message):
-    try:
-        rv = requests.get(url + 'logout', cookies=cookies.get('cookie'))
-        rv = rv.json()
-        cookies.clear()
-        bot.send_message(message.from_user.id, rv.get('message'))
-    except Exception as err:
-        bot.reply_to(message, err)
+    if user_dict.get(message.chat.id):
+        try:
+            rv = requests.get(url + 'logout', cookies=user_dict.get(message.chat.id).token)
+            rv = rv.json()
+            user_dict.pop(message.chat.id)
+            bot.send_message(message.from_user.id, rv.get('message'))
+        except Exception as err:
+            bot.reply_to(message, err)
+    else:
+        bot.send_message(message.from_user.id, 'You have to be logged in.')
 
 
 @bot.message_handler(commands=['search'])
 def handle_search_command(message):
-    if cookies:
+    if user_dict.get(message.chat.id):
         try:
             msg = bot.reply_to(message, 'Please, enter a description of your passport in order to find the password')
             bot.register_next_step_handler(msg, search_pass)
@@ -212,7 +242,8 @@ def handle_search_command(message):
 def search_pass(message):
     try:
         condition = message.text
-        rv = requests.post(url + 'user/passwords/search', json=dict(condition=condition), cookies=cookies.get('cookie'))
+        rv = requests.post(url + 'user/passwords/search', json=dict(condition=condition),
+                           cookies=user_dict.get(message.chat.id).token)
         passwords = rv.json()
 
         for pas in passwords.get('passwords'):
@@ -248,7 +279,7 @@ def callback_query(call):
 
 @bot.message_handler(commands=['edit_pass_info'])
 def handle_edit_pass_command(message):
-    if cookies:
+    if user_dict.get(message.chat.id):
         try:
             user_markup = telebot.types.ReplyKeyboardMarkup()
             user_markup.row('/change_url', '/change_title')
@@ -334,7 +365,7 @@ def get_description(message):
             login=user_pass.login,
             password=user_pass.password,
             comment=user_pass.comment
-        ), cookies=cookies.get('cookie'))
+        ), cookies=user_dict.get(message.chat.id).token)
         bot.send_message(message.from_user.id, rv)
     except Exception as err:
         bot.reply_to(message, err)
@@ -342,10 +373,10 @@ def get_description(message):
 
 @bot.message_handler(func=lambda message: re.match(r'/\d+', message.text))
 def handle_get_particular_pass_command(message):
-    if cookies:
+    if user_dict.get(message.chat.id):
         try:
             # removed / after passwords because message.text will be smth like /digit
-            rv = requests.get(url + 'user/passwords' + message.text, cookies=cookies.get('cookie'))
+            rv = requests.get(url + 'user/passwords' + message.text, cookies=user_dict.get(message.chat.id).token)
             rv = rv.json().get('password')
             bot.send_message(message.from_user.id, f'{rv}')
             chat_id = message.chat.id
@@ -364,11 +395,11 @@ def handle_get_particular_pass_command(message):
 
 @bot.message_handler(func=lambda message: re.match('\N{House Building}', message.text))
 def handle_home(message):
-    if not cookies:
+    if not user_dict.get(message.chat.id):
         user_markup = telebot.types.ReplyKeyboardMarkup()
         user_markup.row('/login', '/help')
         bot.send_message(message.from_user.id, 'Please, choose what you\'d like to do', reply_markup=user_markup)
-    if cookies:
+    if user_dict.get(message.chat.id):
         user_markup = telebot.types.ReplyKeyboardMarkup()
         user_markup.row('/profile', '/search')
         user_markup.row('/get_passwords', '/logout')
@@ -377,11 +408,12 @@ def handle_home(message):
 
 @bot.message_handler(commands=['delete_password'])
 def handle_delete_password_command(message):
-    if cookies:
+    if user_dict.get(message.chat.id):
         try:
             chat_id = message.chat.id
             # removed / after passwords because message.text will be smth like /digit
-            rv = requests.delete(url + 'user/passwords' + pass_dict.get(chat_id).id, cookies=cookies.get('cookie'))
+            rv = requests.delete(url + 'user/passwords' + pass_dict.get(chat_id).id,
+                                 cookies=user_dict.get(message.chat.id).token)
             rv = rv.json()
             bot.send_message(message.from_user.id, rv.get('message'))
         except Exception as err:
