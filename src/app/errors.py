98a0b525e72548bfd3dd1ff82adf0fd897a43193
Artur Manukian <manukian.artur.94@gmail.com>
2018-09-17 14:49:26 +0300

    Added custom exceptions for HTTP status codes.

--- src/app/errors.py
+++ src/app/errors.py
@@ -1,10 +1,37 @@
-errors = {
-    'UserAlreadyExistError': {
-        'message': "A user with that username already exists.",
-        'status': 409
-    },
-    'ValidationError': {
-        'message': "Unprocessable Entity",
-        'status': 422
-    }
-}
+class BaseError(Exception):
+    """Abstract class of custom exceptions for HTTP status codes"""
+    def __init__(self, message, status_code=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+
+
+class InputDataError(BaseError):
+    """400 Bad Request"""
+    def __init__(self, reason):
+        super(InputDataError, self).__init__(reason, 400)
+
+
+class AuthorizationError(BaseError):
+    """401 Unauthorized"""
+    def __init__(self, reason):
+        super(AuthorizationError, self).__init__(reason, 401)
+
+
+class AccessError(BaseError):
+    """403 Forbidden"""
+    def __init__(self, reason):
+        super(AccessError, self).__init__(reason, 403)
+
+
+class SearchError(BaseError):
+    """404 Not Found"""
+    def __init__(self, reason):
+        super(SearchError, self).__init__(reason, 404)
+
+
+class ProcessingError(BaseError):
+    """422 Unprocessable Entity"""
+    def __init__(self, reason):
+        super(ProcessingError, self).__init__(reason, 422)
